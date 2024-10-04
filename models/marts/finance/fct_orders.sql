35e8de447bc309999f1c8d2d119a2deb612e7b28
Albert Aleksa <albert.data.aws@gmail.com>
2024-10-04 23:14:45 +0000

    Modify fct_orders: to store sum of amount for each order. Refactor other models

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,10 +1,44 @@
-select
-    orders.order_id,
-    orders.customer_id,
-    payments.amount
+with orders as (
+    
+    select *
+    from {{ ref("stg_jaffle_shop__orders") }}
 
-from {{ ref("stg_jaffle_shop__orders") }} orders
+),
 
-join {{ ref("stg_stripe__payments") }} payments
-    using(order_id)
+payments as (
+    
+    select *
+    from {{ ref("stg_stripe__payments") }}
+
+),
+
+orders_payments as (
+    
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by order_id
+
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(orders_payments.amount, 0) as amount
+
+    from orders
+
+    join orders_payments
+        using(order_id)
+
+
+)
+
+select * from final
+
     
